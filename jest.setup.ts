1c39588343e8b3b7c5000076ec78b54d31f593d4
talhazubair21 <talhazubair1598@gmail.com>
2025-01-11 14:32:40 +0500

    removed uncessary code

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
-import { TextEncoder, TextDecoder } from "util";
+import { TextEncoder } from "util";
 import "@testing-library/jest-dom";
 
 global.TextEncoder = TextEncoder;
-// global.TextDecoder = TextDecoder;
